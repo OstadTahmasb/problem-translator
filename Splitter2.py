145af61432b3f538db15993f18d5c63a0dbe980d
OstadTahmasb <arman.tahmasebi345@sharif.edu>
2024-12-12 14:12:18 +0330

    Modified splitter

---
+++ Splitter2.py
@@ -0,0 +1,62 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import pandas as pd
+import re
+
+def is_short_answer(problem):
+    if "a)" in problem and "b)" in problem and "c)" in problem and "d)" in problem:
+        return True
+    if "A)" in problem and "B)" in problem and "C)" in problem and "D)" in problem:
+        return True
+    if "1." in problem and "2." in problem and "3." in problem and "4." in problem:
+        return True
+    if "1)" in problem and "2)" in problem and "3)" in problem and "4)":
+        return True
+    if "a." in problem and "b." in problem and "c." in problem and "d.":
+        return True
+    if "A." in problem and "B." in problem and "C." in problem and "D.":
+        return True
+    return False
+
+figures_regex = r"""\\begin{figure}\[.+\]
+    \\centering
+    \\includegraphics\[.+\]{(?P<path>.+)}
+    \\caption{.+}
+    \\label{.+}
+\\end{figure}"""
+def get_figure_paths(problem):
+    return re.findall(figures_regex, problem)
+
+def get_text(problem):
+    trimmed_text = problem.strip()
+    if problem[-2:] == "\\\\":
+        trimmed_text = trimmed_text[:-2]
+    if problems[1] == ".":
+        trimmed_text = trimmed_text[2:]
+    else:
+        trimmed_text = trimmed_text[3:]
+    return trimmed_text.strip()
+
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0, length_function=len, 
+                                            separators=["\\\\ \\\\"], is_separator_regex=True)
+all_problems = {
+    "year": [],
+    "number": [],
+    "text": [],
+    "is_short_answer": [],
+}
+for year in [1376, 1377, 1384, 1385, 1386, 1387, 1388, 1389, 1401, 1402]:
+    loader = TextLoader("final2/{year}.tex".format(year=str(year)))
+    docs = loader.load()
+    problems = splitter.split_text(docs[0].page_content)
+
+    for number, problem in enumerate(problems):
+        all_problems["year"].append(year)
+        all_problems["number"].append(number+1)
+        all_problems["text"].append(get_text(problem))
+        all_problems["is_short_answer"].append(True)
+        
+df = pd.DataFrame(all_problems)
+print(df.head())
+df.to_csv("final2.csv")
